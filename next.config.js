7be7c9166e5a311c7487c964ce93ce5801340788
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2020-12-18 15:14:39 +1100

    added page redirection

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/53e850dbbea741649af7e63e158ec3cf',
+                permanent: true,
+            },
+        ]
+    },
+}
